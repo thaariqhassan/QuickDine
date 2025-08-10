9e223fda6684e2c8cc7dbf28902b0561f89f2d8b
Thaariq Hassan <thaariq.hassan@gmail.com>
2025-08-10 20:13:29 +0530

    initial errors resolved

--- quickdine/src/main.jsx
+++ quickdine/src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router";
 import Home from "./routes/Home";
 
 const root = document.getElementById("root");
