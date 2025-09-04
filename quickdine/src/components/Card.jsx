808f032eeaa8d9587ff897cf2c6344160c81faeb
Thaariq Hassan <thaariq.hassan@gmail.com>
2025-09-04 13:25:32 +0530

    Rough overview of homepage created

---
+++ quickdine/src/components/Card.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "../componentStyles/Card.css"
+function Card({props}) {
+    console.log(props.heading)
+  return (
+    <div className='card-comp'>
+    <div className='card-heading'>
+      {props.heading}
+    </div>
+    <div className="card-body">
+    {props.content}
+    </div>
+    </div>
+    
+  )
+}
+
+export default Card
--- quickdine/src/routes/Home.jsx
+++ quickdine/src/routes/Home.jsx
@@ -1,8 +1,55 @@
 import React from 'react'
-
+import "../routeStyles/Home.css"
+import Card from '../components/Card'
 function Home() {
+  const features = [
+    {
+      heading : "Explore Restaurants",
+      content : "Discover top-rated restaurants, browse menus, and find the perfect dining spot tailored to your taste—all in one place"
+    },
+    {
+      heading : "Instant Booking Confirmation",
+      content : "Get your table reserved in seconds with real-time confirmation—no waiting, no hassle."
+    },
+    {
+      heading : "Personalized Recommendations",
+      content : "Receive smart dining suggestions based on your cuisine preferences, location, and past bookings."
+    }
+  ];
   return (
-    <div>Home</div>
+    <>
+    <div className = "red-box">
+      <div className='header'>
+        <div className='logo'>QUICKDINE</div>
+        <div className='mid-navbar'>
+          <div className='nav-btn'>Home</div>
+          <div className='nav-btn'>Restaurants</div>
+          <div className='nav-btn'>About us</div>
+          <div className='nav-btn'>Blog</div>
+        </div>
+        <div className='act-btn'>Login</div>
+      </div>
+      <div className='body-section'>
+        <div className="tagline">
+          <strong>Where u eat and enjoy</strong><br></br>
+          <i>Go and sleep</i>
+        </div>
+        <div className="pplimg">
+          <img
+          src='https://www.impruvellc.com/wp-content/uploads/2023/07/Relaxed-Decor-Casual-Dress-Code-800x379.jpg'
+          className='imageppl'></img>
+        </div>
+      </div>
+    </div>
+    <div className="lower-layer">
+      {
+        features.map((feature,index) =>{
+          return(
+          <Card key={index} props={feature}/>
+        )})
+      }
+    </div>
+    </>
   )
 }
 
