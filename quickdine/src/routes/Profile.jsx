740fbcfea68318cebbe825dcabf91df6df0dfb61
Thaariq Hassan <thaariq.hassan@gmail.com>
2025-10-22 22:49:13 +0530

    profile cleanup part-1

--- quickdine/src/routes/Profile.jsx
+++ quickdine/src/routes/Profile.jsx
@@ -8,14 +8,8 @@ function Profile({ handleLogout }) {
     const navigate = useNavigate();
     const name = localStorage.getItem("user_name");
     const email_id = localStorage.getItem("user_email");
-    const profile_id = localStorage.getItem("user_id");
-    const [orderList,setOrderList] = useState([]);
-    //get order history
-    useEffect(() => {
-    axios.get(`http://127.0.0.1:8000/orders/${profile_id}`)
-      .then(res => setOrderList(res.data))
-      .catch(err => console.error(err));
-  }, []);
+  
+    
   return(
     <div className='Profile-layout'> 
       
